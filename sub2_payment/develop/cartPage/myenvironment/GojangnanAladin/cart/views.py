c03a750280e0b0a9fa6cb03e3be248b28ef12d28
Heeming <mikana@cau.ac.kr>
2021-05-29 19:22:58 +0900

    subgroup2-cart::Add additional cart functions
    show number of cart items in navbar, adjusting cart item quantity(increment/decrecment), delete item from cart

--- sub2_payment/develop/cartPage/myenvironment/GojangnanAladin/cart/views.py
+++ sub2_payment/develop/cartPage/myenvironment/GojangnanAladin/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from shop.models import Product
 from .models import Cart,CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -25,7 +25,8 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
+        if cart_item.quantity < cart_item.product.stock:
+            cart_item.quantity += 1
         cart_item.save()
 
     except CartItem.DoesNotExist:
@@ -37,6 +38,23 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart:cart_detail')
 
+def cart_remove(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart:cart_detail')
+
+def full_remove(request, product_id):
+    cart = Cart.objects.get(cart_id= _cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart:cart_detail')
 
 def cart_detail(request, total=0, counter=0, cart_items = None):
     try:
