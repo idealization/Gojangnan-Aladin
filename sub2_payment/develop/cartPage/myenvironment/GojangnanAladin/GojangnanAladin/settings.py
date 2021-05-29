e03a067957bc252de6d49ab59db5c365510fd7c6
Heeming <mikana@cau.ac.kr>
2021-05-29 19:41:03 +0900

    subgroup2-cart::Fix typo

--- sub2_payment/develop/cartPage/myenvironment/GojangnanAladin/GojangnanAladin/settings.py
+++ sub2_payment/develop/cartPage/myenvironment/GojangnanAladin/GojangnanAladin/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     str(BASE_DIR / 'static'),
-)
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'static' / 'media'
 
