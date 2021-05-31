732fd755c177b64925b4bea8da50f8b2695c9ccb
Heeming <mikana@cau.ac.kr>
2021-05-31 16:59:44 +0900

    subgroup2-total:fix login error

--- sub2_payment/develop/totalPage_linked/myenvironment/GojangnanAladin/account/urls.py
+++ sub2_payment/develop/totalPage_linked/myenvironment/GojangnanAladin/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'account'
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- sub2_payment/develop/totalPage_linked/myenvironment/GojangnanAladin/account/views.py
+++ sub2_payment/develop/totalPage_linked/myenvironment/GojangnanAladin/account/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from account.form import UserForm
 
 # Create your views here.
-def login(request):
-    return render(request, 'account/login.html')
+# def login(request):
+#     return render(request, 'account/login.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('shop:allProdCat')
+    else:
+        form = UserForm()
+    return render(request, 'account/signup.html', {'form': form})
